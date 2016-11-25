ed2e4bfaa2d9175a4651e63d797e449d354ab6d9
Alex Wilmer <alex.wilmer@oicr.on.ca>
2016-11-25 15:43:17 -0500

    testing web3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import React from 'react'
+import Web3 from 'web3'
 
-let App = () => <div>blank</div>
+let web3 = new Web3(
+  new Web3.providers.HttpProvider(`https://amini.by.ether.camp:8555/sandbox/685d91d1ca`)
+)
+
+class App extends React.Component {
+  state = { result: {} }
+  componentDidMount() {
+    web3.eth.getBlock(48, (error, result) => {
+      if (!error) this.setState({ result })
+      else console.error(error)
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>React App on github pages</h1>
+        <pre>{JSON.stringify(this.state.result, null, 2)}</pre>
+      </div>
+    )
+  }
+}
 
 export default App
